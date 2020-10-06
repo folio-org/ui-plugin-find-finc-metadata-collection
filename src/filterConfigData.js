3de36673ce7a91f1f5e3f0b1f9c906255f1253d3
Viola Elsenhans <elsenhans@ub.uni-leipzig.de>
2020-10-06 10:08:40 +0200

    translateFilterValues

--- src/filterConfigData.js
+++ src/filterConfigData.js
@@ -1,35 +1,38 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+
 const filterConfig = [
   {
     name: 'selected',
     cql: 'selected',
     values: [
-      { name: 'Yes', cql: 'yes' },
-      { name: 'No', cql: 'no' }
+      { name: <FormattedMessage id="ui-plugin-find-finc-metadata-collection.filterValue.yes" />, cql: 'yes' },
+      { name: <FormattedMessage id="ui-plugin-find-finc-metadata-collection.filterValue.no" />, cql: 'no' }
     ],
   },
   {
     name: 'freeContent',
     cql: 'freeContent',
     values: [
-      { name: 'Yes', cql: 'yes' },
-      { name: 'No', cql: 'no' },
-      { name: 'Undetermined', cql: 'undetermined' }
+      { name: <FormattedMessage id="ui-plugin-find-finc-metadata-collection.filterValue.yes" />, cql: 'yes' },
+      { name: <FormattedMessage id="ui-plugin-find-finc-metadata-collection.filterValue.no" />, cql: 'no' },
+      { name: <FormattedMessage id="ui-plugin-find-finc-metadata-collection.filterValue.undetermined" />, cql: 'undetermined' }
     ],
   },
   {
     name: 'permitted',
     cql: 'permitted',
     values: [
-      { name: 'Yes', cql: 'yes' },
-      { name: 'No', cql: 'no' }
+      { name: <FormattedMessage id="ui-plugin-find-finc-metadata-collection.filterValue.yes" />, cql: 'yes' },
+      { name: <FormattedMessage id="ui-plugin-find-finc-metadata-collection.filterValue.no" />, cql: 'no' }
     ],
   },
   {
     name: 'assigned',
     cql: 'assigned',
     values: [
-      { name: 'Yes', cql: 'yes' },
-      { name: 'No', cql: 'no' }
+      { name: <FormattedMessage id="ui-plugin-find-finc-metadata-collection.filterValue.yes" />, cql: 'yes' },
+      { name: <FormattedMessage id="ui-plugin-find-finc-metadata-collection.filterValue.no" />, cql: 'no' }
     ],
   },
   {
