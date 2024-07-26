dabc3010873ae5302fadbba8a4c388962a935554
elsenhans <elsenhans@users.noreply.github.com>
2024-07-26 12:55:45 +0200

    UIFC-362-use-functional-components

--- src/CollectionSearchContainer.test.js
+++ src/CollectionSearchContainer.test.js
@@ -2,6 +2,7 @@ import { render } from '@folio/jest-config-stripes/testing-library/react';
 import { StripesConnectedSource } from '@folio/stripes/smart-components';
 
 import CollectionSearchContainer from './CollectionSearchContainer';
+import CollectionsView from './CollectionsView';
 
 jest.mock('./CollectionsView', () => jest.fn(() => null));
 
@@ -32,6 +33,8 @@ const stripes = {
 const renderCollectionSearchContainer = (props = {}) => (render(
   <CollectionSearchContainer
     mutator={mutator}
+    onChangeIndex={jest.fn}
+    onSelectRow={jest.fn}
     resources={resources}
     stripes={stripes}
     {...props}
@@ -61,5 +64,72 @@ describe('CollectionSearchContainer component', () => {
       expect.objectContaining({ resources, mutator }),
       'metadataCollections'
     );
+  });
+
+  it('should update the query when querySetter is called', () => {
+    renderCollectionSearchContainer();
+
+    const [collectionsViewProps] = CollectionsView.mock.calls[0];
+    const nsValues = { query: 'test' };
+    collectionsViewProps.querySetter({ nsValues });
+
+    expect(mutator.query.update).toHaveBeenCalledWith(nsValues);
+  });
+
+  it('should fetch more data when handleNeedMoreData is called', () => {
+    renderCollectionSearchContainer();
+
+    const [collectionsViewProps] = CollectionsView.mock.calls[0];
+    const RESULT_COUNT_INCREMENT = 100;
+    collectionsViewProps.onNeedMoreData();
+
+    expect(mockSource.fetchMore).toHaveBeenCalledWith(RESULT_COUNT_INCREMENT);
+  });
+
+  it('should update the query when querySetter is called', () => {
+    renderCollectionSearchContainer();
+
+    const [collectionsViewProps] = CollectionsView.mock.calls[0];
+    const nsValues = { query: 'test' };
+    collectionsViewProps.querySetter({ nsValues });
+
+    expect(mutator.query.update).toHaveBeenCalledWith(nsValues);
+  });
+
+  it('should return the query from resources when queryGetter is called', () => {
+    const customResources = {
+      ...resources,
+      query: { query: 'test' }
+    };
+
+    renderCollectionSearchContainer({ resources: customResources });
+
+    const [collectionsViewProps] = CollectionsView.mock.calls[0];
+    const query = collectionsViewProps.queryGetter();
+
+    expect(query).toEqual({ query: 'test' });
+  });
+
+  it('should pass the correct data to CollectionsView', () => {
+    const customResources = {
+      ...resources,
+      metadataCollections: { records: [{ id: '1' }] },
+    };
+
+    renderCollectionSearchContainer({ resources: customResources });
+
+    const [collectionsViewProps] = CollectionsView.mock.calls[0];
+
+    expect(collectionsViewProps.contentData).toEqual([{ id: '1' }]);
+  });
+
+  it('should call onChangeIndex', async () => {
+    renderCollectionSearchContainer();
+
+    const [collectionsViewProps] = CollectionsView.mock.calls[0];
+    const nsValues = { qindex: 'label' };
+    collectionsViewProps.querySetter({ nsValues });
+
+    expect(mutator.query.update).toHaveBeenCalledWith(nsValues);
   });
 });
